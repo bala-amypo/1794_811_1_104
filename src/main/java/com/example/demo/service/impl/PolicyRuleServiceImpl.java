8c6db6b806b32a9b13028760b0d42e6634d9d563
bala-amypo <bala@amypo.in>
2025-12-21 05:12:22 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/PolicyRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PolicyRuleServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.exception.BadRequestException;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.models.PolicyRule;
 import com.example.demo.repository.PolicyRuleRepository;
 import com.example.demo.service.PolicyRuleService;
