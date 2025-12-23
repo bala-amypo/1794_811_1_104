45d2d9beb9f610e1c9771cd0a9a6639e2874d0c1
bala-amypo <bala@amypo.in>
2025-12-23 08:21:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/ IssueDeviceRequest.java
@@ -0,0 +1,21 @@
+package com.example.demo.dto;
+
+public class IssueDeviceRequest {
+
+    private Long employeeId;
+    private Long deviceItemId;
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public Long getDeviceItemId() {
+        return deviceItemId;
+    }
+    public void setDeviceItemId(Long deviceItemId) {
+        this.deviceItemId = deviceItemId;
+    }
+}
