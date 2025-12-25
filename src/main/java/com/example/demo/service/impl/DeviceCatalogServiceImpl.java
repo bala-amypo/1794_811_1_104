1ff386d066cef138c97e48bb2ede2502f274922e
bala-amypo <bala@amypo.in>
2025-12-25 15:13:06 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DeviceCatalogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DeviceCatalogServiceImpl.java
@@ -2,7 +2,7 @@ package com.example.demo.service.impl;
 
 import com.example.demo.exception.BadRequestException;
 import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.models.DeviceCatalogItem;
+import com.example.demo.model.DeviceCatalogItem;
 import com.example.demo.repository.DeviceCatalogItemRepository;
 import com.example.demo.service.DeviceCatalogService;
 
