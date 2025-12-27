23f4fbadb1b4dbbed191a09a818a73ae4a3eb0a5
bala-amypo <bala@amypo.in>
2025-12-27 05:05:55 +0000

    User commit

--- src/main/java/com/example/demo/repository/DeviceCatalogItemRepository.java
+++ src/main/java/com/example/demo/repository/DeviceCatalogItemRepository.java
@@ -1,13 +1,13 @@
-package com.example.demo.repository;
+//package com.example.demo.repository;
 
-import com.example.demo.model.DeviceCatalogItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+//import com.example.demo.model.DeviceCatalogItem;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
 
-@Repository
-public interface DeviceCatalogItemRepository
-        extends JpaRepository<DeviceCatalogItem, Long> {
+//@Repository
+//public interface DeviceCatalogItemRepository
+  //      extends JpaRepository<DeviceCatalogItem, Long> {
 
-        Optional<DeviceCatalogItem> findByDeviceCode(String deviceCode);
-}
+  //      Optional<DeviceCatalogItem> findByDeviceCode(String deviceCode);
+//}
 
